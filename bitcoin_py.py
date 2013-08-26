5147dae993068a0d6b5f455a934540bd375d974a
jolinhuang <xsjcy2bhzl@gmail.com>
2013-08-26 22:45:37 +0800

    m
    m

--- bitcoin_py.py
+++ bitcoin_py.py
@@ -4,13 +4,13 @@ from flask import request
 import service
 app = Flask(__name__)
 
-parameters=request.form
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/requestData',methods=['POST'])
 def requestData():
+    parameters=request.form
     return service.trades(parameters.get("startTime",""),parameters.get("endTime",""),parameters.get("orignal",""))
 if __name__ == '__main__':
     app.run()
