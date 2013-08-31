7aa22229985f5d89ece36abd518c92745d27cd42
huangzhaolin <xsjcy2bhzl2@gmail.com>
2013-08-31 20:04:22 +0800

    m

--- service.py
+++ service.py
@@ -1,8 +1,26 @@
 __author__ = 'zhaolin.huang'
 import MySQLdb as mysql
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import traceback
 
-cnn=mysql.connect(host="jolinhuang.com",user="root",passwd="********",db="bitcoin")
+cnn=mysql.connect(host="localhost",user="root",passwd="5527193",db="bitcoin")
 cursor=cnn.cursor()
+
+def doSql(sql):
+    log.debug(sql)
+    cursor.execute(sql)
+
+LOG_DIR="/home/log"
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("filter")
+fileTimeHandler = TimedRotatingFileHandler("%s/btc.log"%LOG_DIR, "D", 1)
+fileTimeHandler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s: %(message)s"))
+fileTimeHandler.suffix = "%Y-%m-%d.log"
+log.addHandler(fileTimeHandler)
 
 class BitCoinTrade(object):
     def __init__(self,parameters):
@@ -11,11 +29,11 @@ class BitCoinTrade(object):
 
 def trades(startTime,endTime,orignal):
     try:
-        cursor.execute("select id,date_time,price,num,trade_id,trade_num from trade_data where date_time >= '%(startTime)s' \ "
+        doSql("select id,date_time,price,num,trade_id,trade_num from trade_data where date_time >= '%(startTime)s' \ "
                        "and date_time <='%(endTime)s' and orignal = '%(orignal)s'"%{"startTime":startTime,"endTime":endTime,"orignal":orignal})
         return [zip(("id","date_time","price,num","trade_id","trade_num" ),trade) for trade in cursor.fetchall]
-    except Exception,e:
-        print e
+    except Exception:
+        log.debug(traceback.format_exc())
 
 
 
