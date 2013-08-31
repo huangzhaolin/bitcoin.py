65ea85a084f224533d0bfd19475b1f606ea540e4
huangzhaolin <xsjcy2bhzl2@gmail.com>
2013-08-31 20:36:22 +0800

    m

--- bitcoin_py.py
+++ bitcoin_py.py
@@ -12,7 +12,7 @@ def index():
 @app.route('/queryData.htm',methods=['POST'])
 def requestData():
     parameters=request.form
-    print parameters["startTime"]
+    print parameters
     print request.args
     return json.dumps(service.trades(parameters.get("startTime",""),parameters.get("endTime",""),parameters.get("orignal","")),indent=6)
 if __name__ == '__main__':
