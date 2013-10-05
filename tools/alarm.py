08eaf38eac014e664c18dc503b3250ddb62f6d87
jolinhuang <xsjcy2bhzl@gmail.com>
2013-10-05 14:14:20 +0800

    add

--- tools/alarm.py
+++ tools/alarm.py
@@ -10,7 +10,7 @@ class Alarm(object):
         self.orignal=orignal
     def searchData(self):
         searchdata=self.cursor.execute("select date_time,price,num from trade_data where orignal='%s' and date_time>'%s'"%(self.orignal,time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()-15))))
-        return [ dict(zip(("date_time,price,num"),row)) for row in searchdata ]
+        return [ dict(zip(("date_time,price,num"),row)) for row in searchdata.fetchall() ]
     def sendAlarm(self,**message):
         cmd="echo %(message)s | mail -s %(subject)s 228939139@163.com"
         os.system(cmd%message)
